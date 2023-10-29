4c8df658eee99b3b53812b6049cd5481c1a1ab4a
Ashleigh McLean <ashmclean55@gmail.com>
2023-10-29 14:52:35 -0500

    Added a sprinkle of JavaScript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
-/* Add your JavaScript to this file */
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector('form'); 
+    const email = document.querySelector('#email'); 
+    const message = document.querySelector('.message'); 
+
+   form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+
+        const userEmail = email.value.trim();
+
+        if (userEmail.value.includes('@') && userEmail.value.includes('.com')) {
+            // Display a success message
+            message.textContent = `Thank you! Your email address ${userEmail} has been added to our mailing list!`;
+            message.style.color = 'green';
+            //console.log('submitted');
+        } else {
+            // Display an error message for no email provided
+            message.textContent = 'Please enter a valid email address';
+            message.style.color = 'red';
+        }
+
+        message.style.display = 'block';
+    });
+});
